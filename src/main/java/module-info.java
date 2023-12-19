428d141672b46defad454667c81ef6562831ead1
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-12-19 23:27:24 +0600

    gfhgf

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.example.warehousefrontend {
     requires javafx.fxml;
     requires javafx.media;
     requires com.fasterxml.jackson.databind;
+    opens com.example.warehousefrontend to com.fasterxml.jackson.databind, javafx.fxml;
 
-    opens com.example.warehousefrontend to javafx.fxml;
     exports com.example.warehousefrontend;
 }
